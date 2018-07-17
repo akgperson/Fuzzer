380736fd8dd84eb57456f954f93231c015a1aa4d
Allison Ghuman <ahumanlamp@gmail.com>
2018-07-17 14:38:49 -0700

    new binary

--- loop.sh
+++ loop.sh
@@ -6,7 +6,7 @@ for i in $(seq 1 100); do
     tmpfile=/tmp/boolean$$.$i.smt2
     python3 ./boolean.py > $tmpfile 
     echo $tmpfile
-    ./cvc4-2018-06-25-x86_64-linux-dbg --check-models --check-unsat-cores $tmpfile | tee -a counter.txt
+    ./cvc4-07-17-2018 --check-models --check-unsat-cores $tmpfile | tee -a counter.txt
 done
 all=$(grep -o 'sat' counter.txt | wc -l)
 nUnsat=$(grep -o 'unsat' counter.txt | wc -l)
