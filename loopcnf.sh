079ec407beeb73e32362396c423fbfe3294c6904
Allison Ghuman <ahumanlamp@gmail.com>
2018-07-17 14:06:10 -0700

    little issues (using wrong program for input generation, issue with counter file)

--- loopcnf.sh
+++ loopcnf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 rm count.txt
-count.txt
+touch count.txt
 for i in $(seq 1 100); do
     tmpfile=/tmp/booln$$.$i.smt2
-    python3 ./boolean.py > $tmpfile 
+    python3 ./cnf.py > $tmpfile 
     echo $tmpfile
     ./cvc4-2018-06-25-x86_64-linux-dbg --check-models --check-unsat-cores $tmpfile | tee -a count.txt
 done
