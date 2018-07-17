600e5d75d091437de8c5c634fd15db71a2e681ac
Allison Ghuman <ahumanlamp@gmail.com>
2018-07-17 13:53:43 -0700

    gives sat ratio

--- loopcnf.sh
+++ loopcnf.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+rm count.txt
+count.txt
 for i in $(seq 1 100); do
-    tmpfile=/tmp/bool$$.$i.smt2
-    python3 ./cnf.py > $tmpfile 
+    tmpfile=/tmp/booln$$.$i.smt2
+    python3 ./boolean.py > $tmpfile 
     echo $tmpfile
-    ./cvc4-2018-06-25-x86_64-linux-dbg --check-models --check-unsat-cores $tmpfile 
+    ./cvc4-2018-06-25-x86_64-linux-dbg --check-models --check-unsat-cores $tmpfile | tee -a count.txt
 done
+nSAT=$(grep -o 'sat' count.txt | wc -l)
+nUNSAT=$(grep -o 'unsat' count.txt | wc -l)
+echo number sat: $nSAT
+echo number unsat: $nUNSAT
+ratio=$(((nSAT*100) / (nSAT+nUNSAT)))
+echo percent sat: $ratio
