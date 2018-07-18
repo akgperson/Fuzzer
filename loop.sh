1d7c86088d1e2e5d76d0ad033f979ec2968497ad
Allison Ghuman <ahumanlamp@gmail.com>
2018-07-18 12:51:52 -0700

    added timeouts

--- loop.sh
+++ loop.sh
@@ -6,7 +6,7 @@ for i in $(seq 1 100); do
     tmpfile=/tmp/boolean$$.$i.smt2
     python3 ./boolean.py > $tmpfile 
     echo $tmpfile
-    ./cvc4-07-17-2018 --check-models --check-unsat-cores $tmpfile | tee -a counter.txt
+    timeout 10s ./cvc4-07-17-2018 --check-models --check-unsat-cores $tmpfile | tee -a counter.txt
 done
 all=$(grep -o 'sat' counter.txt | wc -l)
 nUnsat=$(grep -o 'unsat' counter.txt | wc -l)
--- loopce.sh
+++ loopce.sh
@@ -6,13 +6,13 @@ for i in $(seq 1 100); do
     tmpfile=/tmp/cexp$$.$i.smt2
     python3 ./CNFexp.py > $tmpfile 
     echo $tmpfile
-    ./cvc4-07-17-2018 --check-models --check-unsat-cores $tmpfile | tee -a c.txt
+    timeout 10s ./cvc4-07-17-2018 --check-models --check-unsat-cores $tmpfile | tee -a c.txt
 done
-all=$(grep -o 'sat' c.txt | wc -l)
+a=$(grep -o 'sat' c.txt | wc -l)
 nUnsat=$(grep -o 'unsat' c.txt | wc -l)
-nSat=$((all - nUnsat))
+nSat=$((a - nUnsat))
 echo number sat: $nSat
 echo number unsat: $nUnsat
-Ratio=$(((nSat*100) / (all)))
-echo percent sat: $Ratio
-echo $Ratio >> percents_CNFexp.txt
+r=$(((nSat*100) / (a)))
+echo percent sat: $r
+echo $r >> percents_CNFexp.txt
